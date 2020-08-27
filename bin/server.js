2a9f6c2d2d2a217fbfe91a02393b5d42ce4eac79
dcbon <dcbonitaa@gmail.com>
2020-08-27 09:52:14 +0700

    fix cors again

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,7 @@
 const http = require('http');
 const app = require('../app');
 const PORT = process.env.PORT || 3000;
-const cors = require('cors')
 
-
-app.use(cors())
 app.set('port', PORT);
 
 const server = http.createServer(app);
