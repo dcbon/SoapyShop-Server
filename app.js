a4135024025c27c8a9c6f2e28fc6d24ce725b9a5
dcbon <dcbonitaa@gmail.com>
2020-08-27 09:44:58 +0700

    fix cors

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes')
 const errHandler = require('./middlewares/errHandler')
-const cors = require('cors')
 
-
-app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 app.use(routes)
--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('../app');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
 
+
+app.use(cors())
 app.set('port', PORT);
 
 const server = http.createServer(app);
